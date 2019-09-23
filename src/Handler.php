8de1d447fd40dfc40a9c206348786a420cdf2f0d
Fauz <fauzy.abdillah@gmail.com>
2019-09-23 15:19:11 +0700

    Update Handler.php

--- src/Handler.php
+++ src/Handler.php
@@ -12,7 +12,54 @@ class Handler
      * @param $data
      * @return
      */
-    public function handle($data) {
-        include 'index.php';
-    }
+    <?php
+/*
+ * Converts CSV to JSON
+ * Example uses Google Spreadsheet CSV feed
+ * csvToArray function I think I found on php.net
+ */
+header('Content-type: application/json');
+// Set your CSV feed
+$feed = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSmqL4GeUW0-H3TzWNuJsPRmKmVYzO1zOvOwZjAtg0gJwZwThlQs3KOiNxGLUxVA54N_kqkOShEP19u/pub?gid=0&single=true&output=csv';
+// Arrays we'll use later
+$keys = array();
+$newArray = array();
+// Function to convert CSV into associative array
+function csvToArray($file, $delimiter) { 
+  if (($handle = fopen($file, 'r')) !== FALSE) { 
+    $i = 0; 
+    while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) { 
+      for ($j = 0; $j < count($lineArray); $j++) { 
+        $arr[$i][$j] = $lineArray[$j]; 
+      } 
+      $i++; 
+    } 
+    fclose($handle); 
+  } 
+  return $arr; 
+} 
+// Do it
+$data = csvToArray($feed, ',');
+// Set number of elements (minus 1 because we shift off the first row)
+$count = count($data) - 1;
+  
+//Use first row for names  
+$labels = array_shift($data);  
+foreach ($labels as $label) {
+  $keys[] = $label;
+}
+// Add Ids, just in case we want them later
+$keys[] = 'id';
+for ($i = 0; $i < $count; $i++) {
+  $data[$i][] = $i;
+}
+  
+// Bring it all together
+for ($j = 0; $j < $count; $j++) {
+  $d = array_combine($keys, $data[$j]);
+  $newArray[$j] = $d;
+}
+// Print it out as JSON
+echo json_encode($newArray);
+?>
 }
